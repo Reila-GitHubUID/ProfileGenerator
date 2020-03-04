1547c4fc310bfc11056114f1550f640d9c6d1f2b
Ellin Tj <tjellin@gmail.com>
2020-03-03 20:48:21 -0800

    constructing user input section

--- Develop/index.js
+++ Develop/index.js
@@ -1,9 +1,3 @@
-console.log("What is your Github user name?");
-const githubUID = process.argv[2];
-
-console.log("What is your favorite color?");
-const faveColor = ;
-
 const fs = require("fs");
 const util = require("util");
 
@@ -22,33 +16,27 @@ function init() {
 
 init();
 
-///////////////////////
-const fs = require("fs");
-const util = require("util");
+////////////////////
+var inquirer = require("inquirer");
 
-const readFileAsync = util.promisify(fs.readFile);
-const writeFileAsync = util.promisify(fs.writeFile);
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is your Github user name?",
+      name: "username"
+    },
+    {
+      type: "rawlist",
+      message: "What is your favorite color?", 
+      choices: ["Green", "Blue", "Pink", "Red"],
+      name: "faveColor"
+    }
+  ])
+  .then(function(response) {
 
-async function combineAnimals() {
-  try {
-    // read from files
-    const cat = await readFileAsync('cat.json', 'utf8'); // read cat.json
-    const dog = await readFileAsync('dog.json', 'utf8'); // read dog.json
-    const goldfish = await readFileAsync('goldfish.json', 'utf8'); // read goldfish.json
-    const hamster = await readFileAsync('hamster.json', 'utf8'); // read hamster.json
+    console.log(response);
 
-    const animals = [JSON.parse(cat), JSON.parse(dog), JSON.parse(goldfish), JSON.parse(hamster)];
-    console.log("Animals: " + animals);
-
-    // write to animals.json
-    await writeFileAsync("animals.json", JSON.stringify(animals, null, 2))
-      .then(function() {
-        console.log("Successfully wrote to animals.json file");
-      });
-
-  } catch(err) {
-    console.log("error message: " + err);
-  }
-}
-
-combineAnimals();
+    // Check the favorite color
+    console.log("My fave color is " + response.faveColor);
+  });
